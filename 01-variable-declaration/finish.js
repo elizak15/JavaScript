2211ce63e4850129d42c57b19bf1d3601db8cbaf
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-07 17:09:22 +0200

    add task1

---
+++ 01-variable-declaration/finish.js
@@ -0,0 +1,24 @@
+/** TASK 1 - Variable Declaration
+ *
+ * 1. Declare the variable "myCountry" and assign it the value -
+ * a string with your country
+ *
+ * 2. It is not planned to assign a new value to this variable in the future
+ *
+ * 3. Print the value of the variable to the console
+ */
+
+
+/** ЗАДАЧА 1 - Объявление переменной
+ *
+ * 1. Объявите переменную "myCountry" и присвойте ей значение -
+ * строку с вашей страной
+ *
+ * 2. В дальнейшем не планируется присваивание нового значения этой переменной
+ *
+ * 3. Выведите значение переменной в консоль
+ */
+
+const myCountry = "Ukraine";
+
+console.log(myCountry)
