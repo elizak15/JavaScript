2e2d1b098fdf3502e9a3f84a1c2d4616ced39564
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-08 01:52:00 +0200

    add task13

---
+++ 13-string-length/finish.js
@@ -0,0 +1,21 @@
+/** TASK 13 - String Length
+ *
+ * 1. Create a variable and assign any string to it
+ *
+ * 2. Display the length of this string in the console
+ */
+
+/** ЗАДАЧА 13 - Длина строки
+ *
+ * 1. Создайте переменную и присвойте ей любую строку
+ *
+ * 2. Виведите в консоль длину этой строки
+ */
+
+
+
+const myName = "Yelyzaveta";
+
+const stringLength = myName.length;
+
+console.log(stringLength)
