e9ac5168ba5843a7a5cf67339b1ea68cea377784
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-08 23:04:00 +0200

    add task20

---
+++ 20-date/finish.js
@@ -0,0 +1,20 @@
+/** TASK 20 - Date
+ *
+ * 1. Create a variable and assign it the current time in milliseconds.
+ *
+ * 2. Output the value to the console
+ */
+
+/** ЗАДАЧА 20 - Дата
+ *
+ * 1. Создайте переменную и присвойте ей текущее время в миллисекундах
+ *
+ * 2. Выведите значение в консоль
+ */
+
+
+const dateNow = new Date();
+
+const dateNowMs = dateNow.getTime();
+
+console.log(dateNowMs)
