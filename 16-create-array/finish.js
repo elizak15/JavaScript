4fc8d86c21904567601c8875fa600d465435823b
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-08 22:38:37 +0200

    add task16

---
+++ 16-create-array/finish.js
@@ -0,0 +1,23 @@
+/** TASK 16 - Creating an Array
+ *
+ * 1. Create an array with three elements of different types
+ *
+ * 2. Print the first element of the array to the console
+ *
+ * 3. Console the length of the array
+ */
+
+/** ЗАДАЧА 16 - Создание массива
+ *
+ * 1. Создайте массив с тремя элементами разных типов
+ *
+ * 2. Выведите в консоль первый элемент массива
+ *
+ * 3. Выведите в консоль длину массива
+ */
+
+const myArray = [true, 10, "str"];
+
+console.log(myArray[0]);
+
+console.log(myArray.length);
