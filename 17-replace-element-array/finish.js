068ac4dcc895e39f4d4ed123d8630a6eab22569f
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-08 22:41:46 +0200

    add task17

---
+++ 17-replace-element-array/finish.js
@@ -0,0 +1,29 @@
+/** TASK 17 - Replacing array elements
+ *
+ * 1. Create an array with several elements of different types
+ *
+ * 2. Output the whole array to the console
+ *
+ * 3. Replace the second element of the array with another one
+ *
+ * 4. Print the modified array to the console
+ */
+
+/** ЗАДАЧА 17 - Замена элементов массива
+ *
+ * 1. Создайте массив с нескольким элементами разных типов
+ *
+ * 2. Выведите в консоль весь массив
+ *
+ * 3. Замените второй элемент массива на другой
+ *
+ * 4. Выведите в консоль измененный массив
+ */
+
+const myArray = [true, 10, "str"];
+
+console.log(myArray);
+
+myArray[1] = 12;
+
+console.log(myArray);
