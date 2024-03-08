d956f9cf15eaf4fcb82b8baf9d9e61289286058e
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-08 22:57:41 +0200

    add task19

---
+++ 19-push-to-array/finish.js
@@ -0,0 +1,24 @@
+/** TASK 19 - Adding elements to an array
+ *
+ * 1. Create any array
+ *
+ * 2. Add some new elements to the end of the array
+ *
+ * 3. Print the length of the resulting array to the console
+ */
+
+/** ЗАДАЧА 19 - Добавление элементов в массив
+ *
+ * 1. Создайте любой массив
+ *
+ * 2. Добавьте в конец массива несколько новых элементов
+ *
+ * 3. Выведите в консоль длину результирующего массива
+ */
+
+
+const myArray = [true, 10, "str"];
+
+myArray.push(null, undefined)
+
+console.log(myArray)
