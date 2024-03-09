e3b0defea825fe3011ee02cd36b1b445d77925c9
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-09 20:14:17 +0200

    add task23

---
+++ 23-or-and-operators/finish.js
@@ -0,0 +1,14 @@
+/** TASK 23 - OR and AND operators
+ *
+ * What will be output to the console?
+ */
+
+/** ЗАДАЧА 23 - ИЛИ и И операторы
+ *
+ * Что будет выведено в консоль?
+ */
+
+console.log(3 || (true && null) || false)
+// && - return null
+//3 || null return 3
+//3 || false return 3
