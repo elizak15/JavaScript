17dc3446857abc37cb22b5e2b82956adea6cb052
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-09 20:50:32 +0200

    add task28

---
+++ 28-let/finish.js
@@ -0,0 +1,15 @@
+/** ЗАДАЧА 28 - Let
+ *
+ * Измените код так, чтобы ошибка исчезла
+ * 
+ */
+
+let myFavoriteAnimal = 'Monkey' // <-- Declaring a variable using const
+
+console.log(myFavoriteAnimal)
+// 'Monkey'
+
+myFavoriteAnimal = 'Cat' //<-- Declaring a variable using const
+
+console.log(myFavoriteAnimal)
+// 'Cat'
