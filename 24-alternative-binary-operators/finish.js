8600f0b12a1b04f8000ec1866d1472094f81278f
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-09 20:14:34 +0200

    add task24

---
+++ 24-alternative-binary-operators/finish.js
@@ -0,0 +1,23 @@
+/** TASK 24 - Alternative Binary Operators
+ *
+ * In each expression, replace two operators with a single binary operator
+ */
+
+/** ЗАДАЧА 24 - Альтернативные бинарные операторы
+ *
+ * В каждом выражении замените два оператора на один бинарный
+ */
+
+let a = 10
+
+a += 1
+console.log(a) // 11
+
+a *= 2
+console.log(a) // 22
+
+a -= 5
+console.log(a) // 17
+
+a /= 2
+console.log(a) // 8.5
