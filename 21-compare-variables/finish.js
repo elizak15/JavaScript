8fc1304d2a468eaa1f1173cb79a2ef595b440948
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-09 18:09:16 +0200

    add task21

---
+++ 21-compare-variables/finish.js
@@ -0,0 +1,31 @@
+/** TASK 21 - Comparison of Variables
+ *
+ * 1. Compare 2 variables "myVariable1" and "myVariable2".
+ * Print "true" to the console if the value of "myVariable1" is less than or equal to "myVariable2".
+ *
+ * 2. Convert both variables to numbers before comparing.
+ */
+
+/** ЗАДАЧА 21 - Сравнение переменных
+ *
+ * 1. Сравните 2 переменные "myVariable1" и "myVariable2".
+ * Выведите в консоль "true", если значение "myVariable1" меньше или равно "myVariable2".
+ *
+ * 2. Преобразуйте обе переменные в числа перед сравнением.
+ */
+
+let myVariable1 = 10
+let myVariable2 = '5'
+
+// Напишите код здесь
+
+console.log(parseInt(myVariable1) <= parseInt(myVariable2))
+
+
+myVariable1 = '20'
+myVariable2 = 100
+
+
+// Напишите код здесь
+
+console.log(+myVariable1 <= +myVariable2)
