8f0e4e6ed37fbdfd31de53a40fa3e218c6f90cdd
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-10 11:59:07 +0200

    add task33

---
+++ 33-add-at-index-array/finish.js
@@ -0,0 +1,25 @@
+/** TASK 33 - Adding an item by a specific index
+ *
+ * 1. Add the element "abc" with index 10.
+ *
+ * 2. Print the resulting array to the console. Explain the results. // 8 empty items before "abc"
+ *
+ * * 3. What is the length of the resulting array? //11
+ */
+
+
+/** ЗАДАЧА 33 - Добавление элемента по определенному индексу
+ *
+ * 1. Добавить элемент "abc" с индексом 10.
+ *
+ * 2. Выведите результирующий массив в консоль. Объясните результаты.
+ *
+ * 3. Какова длина конечного массива?
+ */
+
+const myArray = [1, 2];
+
+myArray[10] = "abc";
+
+console.log(myArray)
+console.log(myArray.length);
