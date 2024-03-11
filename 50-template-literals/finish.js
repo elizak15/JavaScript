2bc0ec7487cb08144f958cb501e37df70ce151e1
Yelyzaveta <kurinna.elizaveta15@gmail.com>
2024-03-11 22:22:24 +0200

    add task50

---
+++ 50-template-literals/finish.js
@@ -0,0 +1,43 @@
+/** TASK 50 - Template Strings
+ *
+ * 1. Create a function "templateLiteral" with one parameter "num"
+ *
+ * 2. The function should return a multi-line string.
+ *
+ * 3. The following are examples of function calls.
+ */
+
+/** ЗАДАЧА 50 - Шаблонные строки
+ *
+ * 1. Создайте функцию "templateLiteral" с одним параметром "num"
+ *
+ * 2. Функция должна возвращать многострочную строку.
+ *
+ * 3. Ниже приведены примеры вызовов функции.
+ */
+
+
+
+const templateLiteral = (num) => {
+    return `Число ${num}
+Это число ${num < 10 ? 'меньше' : 'больше или равно'} 10
+Квадратный корень этого числа - ${Math.sqrt(num)}`
+
+}
+// ТЕСТ 1
+const myNumber = 9
+console.log(templateLiteral(myNumber))
+/* 
+Число 9.
+Это число меньше 10.
+Квадратный корень этого числа - 3.
+*/
+
+// ТЕСТ 2
+const myAnotherNumber = 25
+console.log(templateLiteral(myAnotherNumber))
+/* 
+Число 25.
+Это число больше или равно 10.
+Квадратный корень этого числа - 5.
+*/
