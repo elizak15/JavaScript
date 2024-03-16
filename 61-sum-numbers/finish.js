6ef71bc46a611890dba61dbf8d3b36a90359e838
Liza <kurinna.elizaveta15@gmail.com>
2024-03-16 15:16:24 +0200

    add task61

---
+++ 61-sum-numbers/finish.js
@@ -0,0 +1,32 @@
+/** ЗАДАЧА 61 - Сумма чисел
+ *
+ * 1. Создайте функцию "sumNumbers", которая будет суммировать все переданные ей аргументы.
+ * Количество аргументов неизвестно
+ *
+ * 2. Используйте console.log внутри функции для вывода результата
+ *
+ * 3. Также верните эту сумму как результат функции
+ */
+
+//OPTION 1
+// const sumNumbers = (...args) => {
+//     return console.log(args.reduce((sum, element) => sum + element))
+// }
+
+//OPTION 2
+function sumNumbers() {
+    console.log(arguments);
+    let sum = 0;
+    for(let i = 0; i < arguments.length; i++){
+        sum += arguments[i];
+    }
+    return console.log(sum)
+}
+sumNumbers(1, 3)
+// 4
+
+sumNumbers(10, 20, 5)
+// 35
+
+sumNumbers(2, 5, 80, 1, 10, 12)
+// 110
