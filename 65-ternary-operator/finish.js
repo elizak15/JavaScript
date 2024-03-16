c7ac7bc2ac9b68120203ca37a2b1f56c478a28d4
Liza <kurinna.elizaveta15@gmail.com>
2024-03-16 15:53:58 +0200

    add task65

---
+++ 65-ternary-operator/finish.js
@@ -0,0 +1,32 @@
+/** CHALLENGE 65 - Ternary Operator
+ *
+ * Modify the "isNumber" function using the ternary operator
+ */
+
+
+/** ЗАДАЧА 65 - Тернарный оператор
+ *
+ * Измените функцию "isNumber", используя тернарный оператор
+ */
+
+
+// function isNumber(a) {
+//   if (typeof a === 'number') {
+//     return `${a} - это число`
+//   } else {
+//     return `${a} - это не число`
+//   }
+// }
+
+function isNumber(a) {
+   return typeof a === 'number' ? `${a} - это число` : `${a} - это не число`
+  }
+
+console.log(isNumber(10))
+// 10 - это число
+
+console.log(isNumber('Привет'))
+// Привет - это не число
+
+console.log(isNumber(true))
+// true - это не число
