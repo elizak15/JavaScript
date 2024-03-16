f4d70946fa1f258cbcdf3f1de7c2ef58ce9b0ab4
Liza <kurinna.elizaveta15@gmail.com>
2024-03-16 12:37:49 +0200

    add task54

---
+++ 54-array-destructuring/finish.js
@@ -0,0 +1,47 @@
+/** TASK 54 - Array Destructuring
+ *
+ * Change the parameter section in the "processQuantities" function so that,
+ * so that the outputs in the console match
+ *
+ * Use array destructuring
+ */
+
+/** ЗАДАЧА 54 - Деструктуризация массива
+ *
+ * Измените раздел параметров в функции "processQuantities" так,
+ * чтобы совпали выводы в консоли
+ *
+ * Используйте деструктуризацию массива
+ */
+
+
+//OPTION 1 
+// const processQuantities = (arr) => {
+//   const [minQty, maxQty, defaultQty = 0] = arr;
+//   console.log(minQty) // 8
+//   console.log(maxQty) // 29
+//   console.log(defaultQty) // 10 в первом вызове, 0 во втором вызове
+//   return defaultQty + maxQty - minQty
+// }
+
+// const inputQuantities1 = [8, 29, 10]
+// console.log(processQuantities(inputQuantities1)) // 31
+
+// const inputQuantities2 = [8, 29]
+// console.log(processQuantities(inputQuantities2)) // 21
+
+
+//OPTION 2
+const processQuantities = ([minQty, maxQty, defaultQty = 0]) => {
+  console.log(minQty) // 8
+  console.log(maxQty) // 29
+  console.log(defaultQty) // 10 в первом вызове, 0 во втором вызове
+  return defaultQty + maxQty - minQty
+}
+
+const inputQuantities1 = [8, 29, 10]
+console.log(processQuantities(inputQuantities1)) // 31
+
+const inputQuantities2 = [8, 29]
+console.log(processQuantities(inputQuantities2)) // 21
+
