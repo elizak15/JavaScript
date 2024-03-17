fedc39102537297ca459b91b238c97f7e60a1c13
Liza <kurinna.elizaveta15@gmail.com>
2024-03-17 13:57:53 +0200

    add task76

---
+++ 76-swap-values/finish.js
@@ -0,0 +1,24 @@
+/** TASK 76 - Swap the values of two variables
+ *
+ * Swap the values of variables "a" and "b".
+ * Do not use any new variables to do this.
+ */
+
+
+/** ЗАДАЧА 76 - Поменять местами значения двух переменных
+ *
+ * Поменяйте местами значения переменных "a" и "b".
+ * Не используйте для этого какие-либо новые переменные.
+ */
+
+let a = 'first'
+let b = 'second'
+
+console.log(a, b);
+// first second
+
+[a, b] = [b, a]
+
+
+console.log(a, b)
+// second first
